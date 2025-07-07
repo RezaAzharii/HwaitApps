fa62e80092fde63cee1cc682ce956d69630d923f
RezaAzharii <rezaazhari867@gmail.com>
2025-07-07 20:48:20 +0700

    menambahkan auth response model

---
+++ lib/data/model/response/auth_response_model.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+class AuthResponseModel {
+    final int? statusCode;
+    final String? message;
+    final Data? data;
+
+    AuthResponseModel({
+        this.statusCode,
+        this.message,
+        this.data,
+    });
+
+    factory AuthResponseModel.fromJson(String str) => AuthResponseModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory AuthResponseModel.fromMap(Map<String, dynamic> json) => AuthResponseModel(
+        statusCode: json["status_code"],
+        message: json["message"],
+        data: json["data"] == null ? null : Data.fromMap(json["data"]),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "status_code": statusCode,
+        "message": message,
+        "data": data?.toMap(),
+    };
+}
+
+class Data {
+    final int? id;
+    final String? name;
+    final String? email;
+    final String? role;
+    final String? token;
+
+    Data({
+        this.id,
+        this.name,
+        this.email,
+        this.role,
+        this.token,
+    });
+
+    factory Data.fromJson(String str) => Data.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory Data.fromMap(Map<String, dynamic> json) => Data(
+        id: json["id"],
+        name: json["name"],
+        email: json["email"],
+        role: json["role"],
+        token: json["token"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "email": email,
+        "role": role,
+        "token": token,
+    };
+}
